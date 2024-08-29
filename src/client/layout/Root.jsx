5267d06f3e9ac408a3f17fdec24b5207b85454d9
gavriel <gbatyrov10@gmail.com>
2024-08-29 09:14:31 -0400

    changed designs

--- src/client/layout/Root.jsx
+++ src/client/layout/Root.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
 import "./Root.less";
-import Footer from "./fOOTER.JSX";
+import Footer from "./Footer.JSX";
 
 export default function Root() {
   return (
