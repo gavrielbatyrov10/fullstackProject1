25afab6008ba4d06a18ab33eabf7940687e3af17
gavriel <gbatyrov10@gmail.com>
2024-08-29 09:34:04 -0400

    fixing bug

--- src/client/layout/Root.jsx
+++ src/client/layout/Root.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
 import "./Root.less";
-import Footer from "./layout/Footer.jsx";
+import Footer from "./fOOTER.JSX";
 
 export default function Root() {
   return (
